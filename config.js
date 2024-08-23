4dfb283d8bc4eb190c329ffa8d01a1580682a8c5
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-08-23 14:11:37 +0300

    авпроьб

--- config.js
+++ config.js
@@ -14,44 +14,26 @@ global.authFile = `MysticSession`;
 global.isBaileysFail = false
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['573012482597', '💫 Mystic - OfcDiego', true],
-  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
-  ['5493794297363', '💫 Mystic - Gabriel Ofc', true], 
-  ['393518398856', '💫 Mystic - Tester Fabri115💫', true],
-  ['212781160203', '💫 Mystic - Saad 💫', true], 
-  ['593968585283'],
-  ['5492266466080'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272'],
-  ['5219992843892'],
-  ['5493795319019'],
-  ['573147616444'], 
-  ['5219991402134'],
-  ['5219996125657'],
-  ['5492215034412'],
-  ['5492964650915'],
-  ['5218444937407']
+  ['+7901033947', '👑 Mystic - Creador 👑', true],
+  ['+7901033947', '💫 Mystic - Collaborator 2 💫', true],
+  ['+7901033947', '💫 Mystic - Collaborator 3 💫', true],
+  ['+7901033947', '💫 Mystic - Collaborator 4 💫', true],
+  ['+7901033947', '💫 Mystic - Collaborator 5 💫', true],
+  ['+7901033947', '💫 Mystic - Collaborator 6 💫', true],
+  ['+7901033947', '💫 Mystic - Rey Endymion 💫', false],
+  ['+7901033947', '💫 Mystic - Collaborator 8 💫', true],
+  ['+7901033947', '💫 Mystic - OfcDiego', true],
+  ['+7901033947', '💫 Mystic - By Alba070503 💫', true],
+  ['+7901033947', '💫 Mystic - Gabriel Ofc', true], 
+  ['+7901033947', '💫 Mystic - Tester Fabri115💫', true],
+  ['+7901033947', '💫 Mystic - Saad 💫', true], 
+  ['+7901033947'],
+  ['+7901033947'],
+  ['+7901033947'],
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['+7901033947'];
+global.prems = ['+7901033947'];
 
 global.packname = 'Sticker';
 global.author = 'The Mystic - Bot';
@@ -92,7 +74,7 @@ global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.waitt = '*_[ ⏳ ] Cargando..._*';
 global.waittt = '*_[ ⏳ ] Cargando..._*';
 global.waitttt = '*_[ ⏳ ] Cargando..._*';
-global.nomorown = '5219993404349';
+global.nomorown = '+7901033947';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
